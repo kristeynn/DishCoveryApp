3f8e1faf033cf5adeb7f774fb7676c7b205b74d0
MAE S. ARIATE <ariatemaes13@gmail.com>
2023-05-13 19:27:19 +0800

    Goods na <3

--- app/src/main/java/com/labactivity/dishcoveryapp/MainActivity.kt
+++ app/src/main/java/com/labactivity/dishcoveryapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.labactivity.dishcoveryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +17,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
         firebaseAuth = FirebaseAuth.getInstance()
+
         binding.textView.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
@@ -28,19 +30,16 @@ class MainActivity : AppCompatActivity() {
             val pass = binding.passET.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty()) {
-
                 firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
                     if (it.isSuccessful) {
                         val intent = Intent(this, HomePage::class.java)
                         startActivity(intent)
                     } else {
-                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-
+                        Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
                     }
                 }
             } else {
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-
             }
         }
     }
@@ -48,9 +47,10 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        if(firebaseAuth.currentUser != null){
-            val intent = Intent(this, MainActivity::class.java)
+        if (firebaseAuth.currentUser != null) {
+            val intent = Intent(this, HomePage::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
