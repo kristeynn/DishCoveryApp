519ddfd84f3df97fad4acff086a512d9f69166d6
MAE S. ARIATE <ariatemaes13@gmail.com>
2023-05-13 15:52:11 +0800

    sign-up to homepage

--- app/src/main/java/com/labactivity/dishcoveryapp/SignUpActivity.kt
+++ app/src/main/java/com/labactivity/dishcoveryapp/SignUpActivity.kt
@@ -33,7 +33,7 @@ class SignUpActivity : AppCompatActivity() {
 
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
                         if (it.isSuccessful) {
-                            val intent = Intent(this, MainActivity::class.java)
+                            val intent = Intent(this, HomePage::class.java)
                             startActivity(intent)
                         } else {
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
